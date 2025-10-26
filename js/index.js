94a507c4406ab9ff5511b62fb1ff7b4c593432a6
Habechian Art Studio <habechianstudio@gmail.com>
2025-10-26 00:56:23 -0300

    ...

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function menuClick() {
-    const botao = document.getElementById('menu-section');
-    if (botao.style.display == 'none') {
-        botao.style.display = 'flex';
-    } else if (botao.style.display != 'none') {
-        botao.style.display = 'none';
-    }
-}
